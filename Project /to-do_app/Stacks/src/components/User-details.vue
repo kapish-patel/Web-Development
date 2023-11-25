f3536b81ac3b22f9106df0153769af0c42776a3f
Kapish-patel <kpatelkapish@gmail.com>
2023-11-25 03:10:28 -0800

    updated style for user-details

--- Project /to-do_app/Stacks/src/components/User-details.vue
+++ Project /to-do_app/Stacks/src/components/User-details.vue
@@ -3,35 +3,35 @@
     <div class="profile-section">
       <div class="user-info">
         <p>
-          <strong>Name:</strong>
+          <strong class="string-label" >Name:</strong>
           <template v-if="!isEditing.name">
             {{ UserInfo.name }} 
             <button class="fa-solid fa-pen-to-square" @click="startEditing('name')"></button>
           </template>
           <template v-else>
-            <input ref="nameInput" v-model="editedDetails.name" type="text" placeholder="Name" @blur="saveChanges('name')"/>
+            <input  class="user-info-input" ref="nameInput" v-model="editedDetails.name" type="text" placeholder="Name" @blur="saveChanges('name')"/>
           </template>
         </p>
 
         <p>
-          <strong>Age:</strong>
+          <strong class="string-label" >Age:</strong>
           <template v-if="!isEditing.age">
             {{ UserInfo.age }} 
             <button class="fa-solid fa-pen-to-square" @click="startEditing('age')"></button>
           </template>
           <template v-else>
-            <input ref="ageInput" v-model.number="editedDetails.age" type="number" placeholder="Age" @blur="saveChanges('age')"/>
+            <input class="user-info-input" ref="ageInput" v-model.number="editedDetails.age" type="number" placeholder="Age" @blur="saveChanges('age')"/>
           </template>
         </p>
 
         <p>
-          <strong>Email:</strong>
+          <strong class="string-label" >Email:</strong>
           <template v-if="!isEditing.email">
             {{ UserInfo.email }} 
             <button class="fa-solid fa-pen-to-square" @click="startEditing('email')"></button>
           </template>
           <template v-else>
-            <input ref="emailInput" v-model="editedDetails.email" type="email" placeholder="Email" @blur="saveChanges('email')"/>
+            <input class="user-info-input" ref="emailInput" v-model="editedDetails.email" type="email" placeholder="Email" @blur="saveChanges('email')"/>
           </template>
         </p>
       </div>
@@ -104,6 +104,67 @@ export default {
     display: flex;
     flex-direction: row;
     justify-content: center;
+    font-family: 'Roboto Mono';
 }
+.profile-section {
+  margin-top: 20px;
+}
+
+.string-label{
+  font-size: 17px;
+}
+
+.user-info-input{
+  font-family: 'Roboto Mono';
+  font-size: 13px;
+}
+
+.user-info {
+  background-color: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.user-info p {
+  margin-bottom: 15px;
+}
+
+strong {
+  color: #c21600;
+}
+
+.fa-solid.fa-pen-to-square {
+  background-color: #6FCCFF;
+  border: 1px solid #3388cc;
+  border-radius: 5px;
+  margin-left: 10px;
+  cursor: pointer;
+}
+
+input[type="text"],
+input[type="number"],
+input[type="email"] {
+  padding: 8px;
+  margin-top: 5px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+input[type="text"]:focus,
+input[type="number"]:focus,
+input[type="email"]:focus {
+  outline: none;
+  border-color: #6FCCFF;
+  box-shadow: 0 0 5px rgba(111, 204, 255, 0.5);
+}
+
+input[type="text"]::placeholder,
+input[type="number"]::placeholder,
+input[type="email"]::placeholder {
+  color: #ccc;
+}
+
 
 </style>
